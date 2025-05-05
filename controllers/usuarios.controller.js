b380092f9d50a4c98d7733d71e0cf89de15817e6
gugosabugo <liviasayurishirotargino@gmail.com>
2025-05-05 15:38:05 -0300

    atualização do cadastrar

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -29,12 +29,15 @@ exports.atualizarUsuario = async (req, res) => {
 exports.cadastrarUsuario = async (req, res) => {
     try {
         const resultado = await mysql.execute(
-            `INSERT INTO users(name, email, password)
-	           	  VALUES(?, ?, ?);`,
+            `INSERT INTO users(first_name, last_name, email, password, birth_date, phone)
+	           	  VALUES(?, ?, ?, ?, ?, ?);`,
               [
-                req.body.name,
+                req.body.first_name,
+                req.body.last_name,
                 req.body.email,
-                req.body.password
+                req.body.password,
+                req.body.birth_date,
+                req.body.phone
               ]
         )
 

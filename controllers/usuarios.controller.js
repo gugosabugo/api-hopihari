2d3156bd2ffd24f884635ecffb6bf4fd55399cb0
gugosabugo <liviasayurishirotargino@gmail.com>
2025-05-26 16:59:27 -0300

    Acesso ao dashboard token

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -11,12 +11,12 @@ exports.atualizarUsuario = async (req, res) => {
 	    		    email = ?,
                     password = ?
 		      WHERE id = ?;`,
-              [
+            [
                 req.body.name,
                 req.body.email,
                 req.body.password,
                 idusuario
-              ]
+            ]
         )
 
         res.status(201).send({
@@ -34,14 +34,14 @@ exports.cadastrarUsuario = async (req, res) => {
         const resultado = await mysql.execute(
             `INSERT INTO users(first_name, last_name, email, password, birth_date, phone)
 	           	  VALUES(?, ?, ?, ?, ?, ?);`,
-              [
+            [
                 req.body.first_name,
                 req.body.last_name,
                 req.body.email,
                 hash,
                 req.body.birth_date,
                 req.body.phone
-              ]
+            ]
         )
 
         res.status(201).send({
@@ -54,37 +54,45 @@ exports.cadastrarUsuario = async (req, res) => {
 }
 
 exports.login = async (req, res) => {
-    try{
+    try {
         const usuario = await mysql.execute(
             `SELECT * FROM users WHERE email = ?;`,
             [req.body.email]);
 
-            if(usuario.lenght == 0){
-                return res.status(401).send({ "Mensagem": "Usuario não encontrado!" })
+        if (usuario.lenght == 0) {
+            return res.status(401).send({ "Mensagem": "Usuario não encontrado!" })
+        }
+
+
+        const match = await bcrypt.compare(req.body.password, usuario[0].password);
+        if (!match) {
+            return res.status(401).send({ "Mensagem": "Senha incorreta!" })
+        }
+
+
+        const token = jwt.sign({
+            id: usuario[0].id,
+            first_name: usuario[0].first_name,
+            last_name: usuario[0].last_name,
+            email: usuario[0].email,
+            birth_date: usuario[0].birth_date,
+            phone: usuario[0].phone,
+            admin: usuario[0].admin
+        }, "senhadojwt");
+
+        return res.status(200).send({
+            "Mensagem": "Login realizado com sucesso!",
+            "token": token,
+            "user": {
+                "first_name": usuario[0].first_name,
+                "last_name": usuario[0].last_name,
+                "email": usuario[0].email,
+                "birth_date": usuario[0].birth_date,
+                "phone": usuario[0].phone,
+                "admin": usuario[0].admin
             }
-
-
-            const match = await bcrypt.compare(req.body.password, usuario[0].password);
-            if(!match){
-                return res.status(401).send({ "Mensagem": "Senha incorreta!" })
-            }
-
-
-            const token = jwt.sign({
-                id: usuario[0].id,
-                first_name: usuario[0].first_name,
-                last_name: usuario[0].last_name,
-                email: usuario[0].email,
-                birth_date: usuario[0].birth_date,
-                phone: usuario[0].phone,
-                admin: usuario[0].admin
-            },"senhadojwt");
-
-            return res.status(200).send({
-                "Mensagem": "Login realizado com sucesso!",
-                "token": token
-            });
-    }catch(error){
+        });
+    } catch (error) {
         return res.status(500).send(error)
     }
 }
